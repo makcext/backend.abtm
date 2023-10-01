6c5d3e86727f0f42fa8738ad231e8b74d3a2e4e4
makcext <makcbowka@gmail.com>
2023-10-01 18:06:07 +0300

    cors guthub pages support add

--- src/server.js
+++ src/server.js
@@ -6,7 +6,7 @@ const app = express();
 app.use(json());
 
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', 'https://localhost:3000');
+  res.setHeader('Access-Control-Allow-Origin', 'https://localhost:3000, https://makcext.github.io');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   next();
