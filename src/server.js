8bb1e71ee76c6eb942834796dbea9b658a8f30d2
makcext <makcbowka@gmail.com>
2023-10-01 03:21:54 +0300

    init

--- src/server.js
+++ src/server.js
@@ -6,6 +6,8 @@ const server = http.createServer((req, res) => {
   res.end();
 });
 
-server.listen(3000, () => {
-  console.log('Server listening on port 3000');
+const port = process.env.PORT || 3000;
+
+server.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
 });
