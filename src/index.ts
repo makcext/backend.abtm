137712004d321be0adea9c3c7b8688bda5c261d8
makcext <makcbowka@gmail.com>
2023-10-07 15:46:40 +0300

    17:00

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,15 @@
 import { ApolloServer } from "@apollo/server";
 import { startStandaloneServer } from "@apollo/server/standalone";
+import { connect } from "mongoose";
+import Book from "../models/book.js";
+
+const MONGODB = 'mongodb+srv://root:root@atlascluster.g9bnlyp.mongodb.net/books?retryWrites=true&w=majority';
+
+const typeDefs = `#graphql
+	type Book {
+		_id: String
+		author: String
+		title: String
+		year: Int
+	}
+`;
