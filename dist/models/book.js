d1e2f508a09c2d6ae6946b0120b9d0800a549ed8
makcext <makcbowka@gmail.com>
2023-11-17 17:57:59 +0200

    seve

--- dist/models/book.js
+++ dist/models/book.js
@@ -4,6 +4,7 @@ const bookSchema = new Schema({
     author: { type: String, required: true },
     title: { type: String, required: true },
     year: { type: Number, required: true },
+    user: { type: Schema.Types.ObjectId, ref: 'User' },
 });
 const Book = model('books', bookSchema);
 export default Book;
